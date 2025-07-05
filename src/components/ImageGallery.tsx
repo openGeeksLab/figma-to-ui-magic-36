67d6a256a110cbac53b80ce73fa84c03f54ef293
openGeeksLab <drudeveldvurech@gmail.com>
2025-07-05 21:56:49 +0200

    Update ImageGallery.tsx

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -32,7 +32,7 @@ const ImageGallery = () => {
                 <span className="text-[#454545]">Any</span>
               </h3>
             </div>
-             </article>
+           
             <div className="flex items-center gap-2.5 bg-white p-2.5">
               <h3 className="text-[#454545] text-[42px] font-bold max-md:text-[28px] max-sm:text-xl">
                 Aesthetic
@@ -41,7 +41,7 @@ const ImageGallery = () => {
           </div>
         </div>
       </div>
-  
+    </article>
       <div className="flex flex-col items-start gap-6 flex-1 h-full max-md:w-full">
         <article className="flex-1 w-full relative">
           <img
