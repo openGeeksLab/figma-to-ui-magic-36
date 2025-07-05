a86c2d9477c71b30270644896533643edbc9ae8c
openGeeksLab <drudeveldvurech@gmail.com>
2025-07-05 22:17:11 +0200

    Update ImageGallery.tsx

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -6,18 +6,18 @@ const ImageGallery = () => {
       <div 
         className="flex w-[961px] h-[1008px] flex-col justify-end items-start gap-2.5 p-10 rounded-[28px] max-md:w-full max-md:h-[600px] max-sm:h-[500px] max-sm:p-5"
         style={{
-          backgroundImage: 'url("URL_4")',
+          backgroundImage: "https://cdn.builder.io/api/v1/image/assets/TEMP/b05f297504b121b19c41286afd42bf86029003c2?width=1358",
           backgroundSize: 'cover',
           backgroundPosition: 'center'
         }}
       >
         <div className="flex flex-col justify-end items-start flex-1 w-full">
-          <article className="flex-1 w-full relative">
+{/*           <article className="flex-1 w-full relative">
           <img
             src="https://cdn.builder.io/api/v1/image/assets/TEMP/b05f297504b121b19c41286afd42bf86029003c2?width=1358"
             alt="Next generation wood cladding example"
             className="w-full h-[492px] object-cover rounded-[28px]"
-          />
+          /> */}
           <div className="flex flex-col items-start">
             <div className="flex items-center gap-2.5 bg-white p-2.5">
               <h3 className="text-[#454545] text-[42px] font-bold max-md:text-[28px] max-sm:text-xl">
@@ -37,7 +37,7 @@ const ImageGallery = () => {
               </h3>
             </div>
           </div>
-            </article>
+{/*             </article> */}
         </div>
       </div>
       
