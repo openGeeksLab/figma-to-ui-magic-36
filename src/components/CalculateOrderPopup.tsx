5194d22243873bae2e561c7e6569e7e9da7b3406
openGeeksLab <drudeveldvurech@gmail.com>
2025-08-10 19:18:08 +0200

    surfaceTreatment

--- src/components/CalculateOrderPopup.tsx
+++ src/components/CalculateOrderPopup.tsx
@@ -125,7 +125,7 @@ const CalculateOrderPopup: React.FC<CalculateOrderPopupProps> = ({ children, pro
           length: formData.length,
           width: formData.width,
           square: formData.square,
-          estimatedPrice: calculatedPrice ? `${calculatedPrice.toFixed(0)} SEK` : undefined
+          estimatedPrice: calculatedPrice ? `${surfaceTreatment} SEK` : undefined
         }
       };
 
@@ -240,7 +240,7 @@ const CalculateOrderPopup: React.FC<CalculateOrderPopupProps> = ({ children, pro
             <div className="flex flex-col justify-center items-center lg:items-start">
               <h3 className="text-xl font-semibold text-[#454545] mb-4">Your prise is:</h3>
               <div className="text-4xl font-bold text-[#DCB481] mb-4">
-                {calculatedPrice ? `${calculatedPrice.toFixed(0)} SEK*` : '-- SEK*'}
+                {calculatedPrice ? `${surfaceTreatment} SEK*` : '-- SEK*'}
               </div>
               <p className="text-sm text-gray-500 text-center lg:text-left">
                 * The calculation is approximate. Leave a request and we will make an exact calculation of your order.
